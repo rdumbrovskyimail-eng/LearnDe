d6fa130218e42b1554477d0d3a3735f2ed28693f
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2026-03-30 01:33:11 +0200

    лвшв

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,8 +79,8 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.8.9")
 
     // Hilt
-    implementation("com.google.dagger:hilt-android:2.54.1")
-    ksp("com.google.dagger:hilt-android-compiler:2.54.1")
+    implementation("com.google.dagger:hilt-android:2.57.1")
+    ksp("com.google.dagger:hilt-android-compiler:2.57.1")
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
 
     // Network
