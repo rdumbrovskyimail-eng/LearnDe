7d682a15cbf68be3d8a91af277613a63d7e6dd06
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2026-03-30 03:46:17 +0200

    укк

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
 
     // Core
     implementation("androidx.core:core-ktx:1.18.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.activity:activity-ktx:1.10.1")
 
     // Compose BOM
