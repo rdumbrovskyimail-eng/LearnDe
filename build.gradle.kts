2548967ea28136c4ad17cb57fc28199a08fb72dd
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2026-03-30 03:31:34 +0200

    рро

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("org.jetbrains.kotlin.android") version "2.1.20" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.1.20" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.1.20" apply false
-    id("com.google.devtools.ksp") version "2.1.20-1.0.29" apply false
+    id("com.google.devtools.ksp") version "2.1.20-1.0.28" apply false
     id("com.google.dagger.hilt.android") version "2.57.1" apply false
 
     id("com.github.ben-manes.versions") version "0.51.0"
