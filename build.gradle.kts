8bb017eb4947a2519585d4a0279313669881c026
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2026-03-30 03:19:55 +0200

    шол

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,18 @@
 plugins {
     id("com.android.application") version "8.9.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.0" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
-    id("com.google.devtools.ksp") version "2.1.0-1.0.28" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.20" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.20" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.1.20" apply false
+    id("com.google.devtools.ksp") version "2.1.20-1.0.29" apply false
     id("com.google.dagger.hilt.android") version "2.57.1" apply false
+
+    id("com.github.ben-manes.versions") version "0.51.0"
+}
+
+tasks.withType<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask> {
+    rejectVersionIf {
+        val isStable = "^[0-9,.v-]+(-r)?$".toRegex().matches(candidate.version)
+        val isUnstable = candidate.version.matches(".*(alpha|beta|rc|RC|m|M).*".toRegex())
+        !isStable && isUnstable
+    }
 }
