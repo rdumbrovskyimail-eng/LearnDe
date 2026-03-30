ab81faf82b1dd27948da0f58c791902be261d7d4
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2026-03-30 04:26:45 +0200

    уддулу

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,17 @@
 plugins {
-    id("com.android.application") version "8.9.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.20" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.20" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.20" apply false
-    id("com.google.devtools.ksp") version "2.1.20-1.0.32" apply false
-    id("com.google.dagger.hilt.android") version "2.57.1" apply false
-
-    id("com.github.ben-manes.versions") version "0.51.0"
+    id("com.android.application")                   version "9.1.0"   apply false
+    id("org.jetbrains.kotlin.android")              version "2.3.20"  apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.3.20"  apply false
+    id("org.jetbrains.kotlin.plugin.compose")       version "2.3.20"  apply false
+    id("com.google.devtools.ksp")                   version "2.3.6"   apply false
+    id("com.google.dagger.hilt.android")            version "2.59.2"  apply false
+    id("com.github.ben-manes.versions")             version "0.53.0"  apply false
 }
 
 tasks.withType<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask> {
     rejectVersionIf {
-        val isStable = "^[0-9,.v-]+(-r)?$".toRegex().matches(candidate.version)
-        val isUnstable = candidate.version.matches(".*(alpha|beta|rc|RC|m|M).*".toRegex())
-        !isStable && isUnstable
+        val isUnstable = candidate.version
+            .matches(".*(alpha|beta|rc|RC|m|M).*".toRegex())
+        isUnstable
     }
 }
