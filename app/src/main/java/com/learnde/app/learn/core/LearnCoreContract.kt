52775534d98a8b4c890fe9ba8ab301640ce136c5
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2026-04-26 14:44:14 +0200

    jqjq

--- app/src/main/java/com/learnde/app/learn/core/LearnCoreContract.kt
+++ app/src/main/java/com/learnde/app/learn/core/LearnCoreContract.kt
@@ -18,6 +18,7 @@ data class LearnCoreState(
     val arbiterOwned: Boolean = false,
     // ФИНАЛ: Флаг для показа красивой анимации загрузки
     val isPreparingSession: Boolean = false, 
+    val isFinishingSession: Boolean = false,
 )
 
 sealed class LearnCoreIntent {
