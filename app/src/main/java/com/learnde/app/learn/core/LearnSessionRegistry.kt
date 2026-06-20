b41000fe4dca888ba955dab3369de2badf8d5b4c
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2026-06-20 14:26:39 +0200

    [Pipeline] LearnSessionRegistry.kt

--- app/src/main/java/com/learnde/app/learn/core/LearnSessionRegistry.kt
+++ app/src/main/java/com/learnde/app/learn/core/LearnSessionRegistry.kt
@@ -10,6 +10,7 @@ package com.learnde.app.learn.core
 import com.learnde.app.learn.sessions.a1.A1ReviewSession
 import com.learnde.app.learn.sessions.a1.A1SituationSession
 import com.learnde.app.learn.sessions.a1.v2.A1AdaptiveSession
+import com.learnde.app.learn.sessions.a1book.A1BookSession
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -18,7 +19,7 @@ class LearnSessionRegistry @Inject constructor(
     a1Learning: A1SituationSession,
     a1Review: A1ReviewSession,
     a1Adaptive: A1AdaptiveSession,
-    a1Book: com.learnde.app.learn.sessions.a1book.A1BookSession,
+    a1Book: A1BookSession,
 ) {
     private val sessions: Map<String, LearnSession> = mapOf(
         a1Learning.id to a1Learning,
