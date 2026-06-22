256c678f188c03d3c7ee1bb1d04fcb6a9b1cc554
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2026-06-22 20:43:38 +0200

    [Pipeline] LearnSessionRegistry.kt

--- app/src/main/java/com/learnde/app/learn/core/LearnSessionRegistry.kt
+++ app/src/main/java/com/learnde/app/learn/core/LearnSessionRegistry.kt
@@ -10,7 +10,7 @@ package com.learnde.app.learn.core
 import com.learnde.app.learn.sessions.a1.A1ReviewSession
 import com.learnde.app.learn.sessions.a1.A1SituationSession
 import com.learnde.app.learn.sessions.a1.v2.A1AdaptiveSession
-// import com.learnde.app.learn.sessions.a1book.A1BookSession
+import com.learnde.app.learn.sessions.a1book.A1BookSession
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -19,13 +19,13 @@ class LearnSessionRegistry @Inject constructor(
     a1Learning: A1SituationSession,
     a1Review: A1ReviewSession,
     a1Adaptive: A1AdaptiveSession,
-    // a1Book: A1BookSession,
+    a1Book: A1BookSession,
 ) {
     private val sessions: Map<String, LearnSession> = mapOf(
         a1Learning.id to a1Learning,
         a1Review.id   to a1Review,
         a1Adaptive.id to a1Adaptive,
-        // a1Book.id     to a1Book,
+        a1Book.id     to a1Book,
     )
 
     fun get(id: String): LearnSession? = sessions[id]
