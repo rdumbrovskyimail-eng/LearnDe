b46b00504a75055bcf95653f671fffde8097178d
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2026-06-24 21:47:06 +0200

    [Pipeline] CREATE ClientScreen.kt

---
+++ app/src/main/java/com/learnde/app/presentation/client/ClientScreen.kt
@@ -0,0 +1,8 @@
+package com.learnde.app.presentation.client
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ClientScreen(viewModel: ClientViewModel) {
+    // TODO: Implement UI
+}
